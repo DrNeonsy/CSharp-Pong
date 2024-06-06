cc754f930dc3012b0a5ef44ebc11ca6b301b8bf8
Musisipi WuzHere <musisipii@outlook.com>
2024-06-06 15:32:46 +0700

    add new ball

--- CSharp-WFA_Pong/Game.Designer.cs
+++ CSharp-WFA_Pong/Game.Designer.cs
@@ -28,194 +28,206 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game));
-            this.playerOne = new System.Windows.Forms.PictureBox();
-            this.gameLoop = new System.Windows.Forms.Timer(this.components);
-            this.scoreLeftSide = new System.Windows.Forms.Label();
-            this.scoreRightSide = new System.Windows.Forms.Label();
-            this.playerTwo = new System.Windows.Forms.PictureBox();
-            this.pongBall = new System.Windows.Forms.PictureBox();
-            this.sep1 = new System.Windows.Forms.PictureBox();
-            this.sep2 = new System.Windows.Forms.PictureBox();
-            this.sep3 = new System.Windows.Forms.PictureBox();
-            this.sep4 = new System.Windows.Forms.PictureBox();
-            this.sep5 = new System.Windows.Forms.PictureBox();
-            this.sep6 = new System.Windows.Forms.PictureBox();
-            this.sep7 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.playerOne)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.playerTwo)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pongBall)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sep1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sep2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sep3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sep4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sep5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sep6)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sep7)).BeginInit();
-            this.SuspendLayout();
+            playerOne = new PictureBox();
+            gameLoop = new System.Windows.Forms.Timer(components);
+            scoreLeftSide = new Label();
+            scoreRightSide = new Label();
+            playerTwo = new PictureBox();
+            pongBall = new PictureBox();
+            sep1 = new PictureBox();
+            sep2 = new PictureBox();
+            sep3 = new PictureBox();
+            sep4 = new PictureBox();
+            sep5 = new PictureBox();
+            sep6 = new PictureBox();
+            sep7 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)playerOne).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)playerTwo).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pongBall).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)sep1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)sep2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)sep3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)sep4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)sep5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)sep6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)sep7).BeginInit();
+            SuspendLayout();
             // 
             // playerOne
             // 
-            this.playerOne.BackColor = System.Drawing.Color.OrangeRed;
-            this.playerOne.Location = new System.Drawing.Point(9, 500);
-            this.playerOne.Margin = new System.Windows.Forms.Padding(0);
-            this.playerOne.Name = "playerOne";
-            this.playerOne.Size = new System.Drawing.Size(6, 150);
-            this.playerOne.TabIndex = 0;
-            this.playerOne.TabStop = false;
+            playerOne.BackColor = Color.OrangeRed;
+            playerOne.Location = new Point(7, 400);
+            playerOne.Margin = new Padding(0);
+            playerOne.Name = "playerOne";
+            playerOne.Size = new Size(5, 120);
+            playerOne.TabIndex = 0;
+            playerOne.TabStop = false;
             // 
             // gameLoop
             // 
-            this.gameLoop.Enabled = true;
-            this.gameLoop.Interval = 5;
-            this.gameLoop.Tick += new System.EventHandler(this.Loop);
+            gameLoop.Enabled = true;
+            gameLoop.Interval = 5;
+            gameLoop.Tick += Loop;
             // 
             // scoreLeftSide
             // 
-            this.scoreLeftSide.BackColor = System.Drawing.Color.Transparent;
-            this.scoreLeftSide.Font = new System.Drawing.Font("Calibri", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.scoreLeftSide.ForeColor = System.Drawing.Color.OrangeRed;
-            this.scoreLeftSide.Location = new System.Drawing.Point(720, 9);
-            this.scoreLeftSide.Name = "scoreLeftSide";
-            this.scoreLeftSide.Size = new System.Drawing.Size(97, 54);
-            this.scoreLeftSide.TabIndex = 1;
-            this.scoreLeftSide.Text = "0";
-            this.scoreLeftSide.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            scoreLeftSide.BackColor = Color.Transparent;
+            scoreLeftSide.Font = new Font("Calibri", 27F, FontStyle.Regular, GraphicsUnit.Point);
+            scoreLeftSide.ForeColor = Color.OrangeRed;
+            scoreLeftSide.Location = new Point(576, 7);
+            scoreLeftSide.Margin = new Padding(2, 0, 2, 0);
+            scoreLeftSide.Name = "scoreLeftSide";
+            scoreLeftSide.Size = new Size(78, 43);
+            scoreLeftSide.TabIndex = 1;
+            scoreLeftSide.Text = "0";
+            scoreLeftSide.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // scoreRightSide
             // 
-            this.scoreRightSide.BackColor = System.Drawing.Color.Transparent;
-            this.scoreRightSide.Font = new System.Drawing.Font("Calibri", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.scoreRightSide.ForeColor = System.Drawing.Color.BlueViolet;
-            this.scoreRightSide.Location = new System.Drawing.Point(1086, 9);
-            this.scoreRightSide.Name = "scoreRightSide";
-            this.scoreRightSide.Size = new System.Drawing.Size(84, 54);
-            this.scoreRightSide.TabIndex = 3;
-            this.scoreRightSide.Text = "0";
-            this.scoreRightSide.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            scoreRightSide.BackColor = Color.Transparent;
+            scoreRightSide.Font = new Font("Calibri", 27F, FontStyle.Regular, GraphicsUnit.Point);
+            scoreRightSide.ForeColor = Color.BlueViolet;
+            scoreRightSide.Location = new Point(869, 7);
+            scoreRightSide.Margin = new Padding(2, 0, 2, 0);
+            scoreRightSide.Name = "scoreRightSide";
+            scoreRightSide.Size = new Size(67, 43);
+            scoreRightSide.TabIndex = 3;
+            scoreRightSide.Text = "0";
+            scoreRightSide.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // playerTwo
             // 
-            this.playerTwo.BackColor = System.Drawing.Color.BlueViolet;
-            this.playerTwo.Location = new System.Drawing.Point(1883, 500);
-            this.playerTwo.Margin = new System.Windows.Forms.Padding(0);
-            this.playerTwo.Name = "playerTwo";
-            this.playerTwo.Size = new System.Drawing.Size(6, 150);
-            this.playerTwo.TabIndex = 5;
-            this.playerTwo.TabStop = false;
+            playerTwo.BackColor = Color.BlueViolet;
+            playerTwo.Location = new Point(1506, 400);
+            playerTwo.Margin = new Padding(0);
+            playerTwo.Name = "playerTwo";
+            playerTwo.Size = new Size(5, 120);
+            playerTwo.TabIndex = 5;
+            playerTwo.TabStop = false;
             // 
             // pongBall
             // 
-            this.pongBall.BackColor = System.Drawing.Color.DeepSkyBlue;
-            this.pongBall.Location = new System.Drawing.Point(926, 461);
-            this.pongBall.Name = "pongBall";
-            this.pongBall.Size = new System.Drawing.Size(21, 21);
-            this.pongBall.TabIndex = 6;
-            this.pongBall.TabStop = false;
+            pongBall.BackColor = Color.Transparent;
+            pongBall.BackgroundImage = (Image)resources.GetObject("pongBall.BackgroundImage");
+            pongBall.BackgroundImageLayout = ImageLayout.Stretch;
+            pongBall.Location = new Point(735, 364);
+            pongBall.Margin = new Padding(2, 2, 2, 2);
+            pongBall.Name = "pongBall";
+            pongBall.Size = new Size(28, 27);
+            pongBall.TabIndex = 6;
+            pongBall.TabStop = false;
             // 
             // sep1
             // 
-            this.sep1.BackColor = System.Drawing.Color.White;
-            this.sep1.Location = new System.Drawing.Point(932, 12);
-            this.sep1.Name = "sep1";
-            this.sep1.Size = new System.Drawing.Size(9, 118);
-            this.sep1.TabIndex = 7;
-            this.sep1.TabStop = false;
+            sep1.BackColor = Color.White;
+            sep1.Location = new Point(746, 10);
+            sep1.Margin = new Padding(2, 2, 2, 2);
+            sep1.Name = "sep1";
+            sep1.Size = new Size(7, 94);
+            sep1.TabIndex = 7;
+            sep1.TabStop = false;
             // 
             // sep2
             // 
-            this.sep2.BackColor = System.Drawing.Color.White;
-            this.sep2.Location = new System.Drawing.Point(932, 171);
-            this.sep2.Name = "sep2";
-            this.sep2.Size = new System.Drawing.Size(9, 118);
-            this.sep2.TabIndex = 8;
-            this.sep2.TabStop = false;
+            sep2.BackColor = Color.White;
+            sep2.Location = new Point(746, 137);
+            sep2.Margin = new Padding(2, 2, 2, 2);
+            sep2.Name = "sep2";
+            sep2.Size = new Size(7, 94);
+            sep2.TabIndex = 8;
+            sep2.TabStop = false;
             // 
             // sep3
             // 
-            this.sep3.BackColor = System.Drawing.Color.White;
-            this.sep3.Location = new System.Drawing.Point(932, 332);
-            this.sep3.Name = "sep3";
-            this.sep3.Size = new System.Drawing.Size(9, 118);
-            this.sep3.TabIndex = 9;
-            this.sep3.TabStop = false;
+            sep3.BackColor = Color.White;
+            sep3.Location = new Point(746, 266);
+            sep3.Margin = new Padding(2, 2, 2, 2);
+            sep3.Name = "sep3";
+            sep3.Size = new Size(7, 94);
+            sep3.TabIndex = 9;
+            sep3.TabStop = false;
             // 
             // sep4
             // 
-            this.sep4.BackColor = System.Drawing.Color.White;
-            this.sep4.Location = new System.Drawing.Point(932, 488);
-            this.sep4.Name = "sep4";
-            this.sep4.Size = new System.Drawing.Size(9, 118);
-            this.sep4.TabIndex = 10;
-            this.sep4.TabStop = false;
+            sep4.BackColor = Color.White;
+            sep4.Location = new Point(746, 390);
+            sep4.Margin = new Padding(2, 2, 2, 2);
+            sep4.Name = "sep4";
+            sep4.Size = new Size(7, 94);
+            sep4.TabIndex = 10;
+            sep4.TabStop = false;
             // 
             // sep5
             // 
-            this.sep5.BackColor = System.Drawing.Color.White;
-            this.sep5.Location = new System.Drawing.Point(932, 642);
-            this.sep5.Name = "sep5";
-            this.sep5.Size = new System.Drawing.Size(9, 118);
-            this.sep5.TabIndex = 11;
-            this.sep5.TabStop = false;
+            sep5.BackColor = Color.White;
+            sep5.Location = new Point(746, 514);
+            sep5.Margin = new Padding(2, 2, 2, 2);
+            sep5.Name = "sep5";
+            sep5.Size = new Size(7, 94);
+            sep5.TabIndex = 11;
+            sep5.TabStop = false;
             // 
             // sep6
             // 
-            this.sep6.BackColor = System.Drawing.Color.White;
-            this.sep6.Location = new System.Drawing.Point(932, 794);
-            this.sep6.Name = "sep6";
-            this.sep6.Size = new System.Drawing.Size(9, 118);
-            this.sep6.TabIndex = 12;
-            this.sep6.TabStop = false;
+            sep6.BackColor = Color.White;
+            sep6.Location = new Point(746, 635);
+            sep6.Margin = new Padding(2, 2, 2, 2);
+            sep6.Name = "sep6";
+            sep6.Size = new Size(7, 94);
+            sep6.TabIndex = 12;
+            sep6.TabStop = false;
             // 
             // sep7
             // 
-            this.sep7.BackColor = System.Drawing.Color.White;
-            this.sep7.Location = new System.Drawing.Point(932, 935);
-            this.sep7.Name = "sep7";
-            this.sep7.Size = new System.Drawing.Size(9, 77);
-            this.sep7.TabIndex = 13;
-            this.sep7.TabStop = false;
+            sep7.BackColor = Color.White;
+            sep7.Location = new Point(746, 748);
+            sep7.Margin = new Padding(2, 2, 2, 2);
+            sep7.Name = "sep7";
+            sep7.Size = new Size(7, 62);
+            sep7.TabIndex = 13;
+            sep7.TabStop = false;
             // 
             // Game
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Desktop;
-            this.ClientSize = new System.Drawing.Size(1898, 1024);
-            this.Controls.Add(this.pongBall);
-            this.Controls.Add(this.sep7);
-            this.Controls.Add(this.sep6);
-            this.Controls.Add(this.sep5);
-            this.Controls.Add(this.sep4);
-            this.Controls.Add(this.sep3);
-            this.Controls.Add(this.sep2);
-            this.Controls.Add(this.sep1);
-            this.Controls.Add(this.playerTwo);
-            this.Controls.Add(this.playerOne);
-            this.Controls.Add(this.scoreRightSide);
-            this.Controls.Add(this.scoreLeftSide);
-            this.ForeColor = System.Drawing.Color.Transparent;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximumSize = new System.Drawing.Size(1920, 1080);
-            this.MinimumSize = new System.Drawing.Size(1920, 1080);
-            this.Name = "Game";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Pong";
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Game_KeyDown);
-            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Game_KeyUp);
-            ((System.ComponentModel.ISupportInitialize)(this.playerOne)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.playerTwo)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pongBall)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sep1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sep2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sep3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sep4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sep5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sep6)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sep7)).EndInit();
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Desktop;
+            ClientSize = new Size(1520, 819);
+            Controls.Add(pongBall);
+            Controls.Add(sep7);
+            Controls.Add(sep6);
+            Controls.Add(sep5);
+            Controls.Add(sep4);
+            Controls.Add(sep3);
+            Controls.Add(sep2);
+            Controls.Add(sep1);
+            Controls.Add(playerTwo);
+            Controls.Add(playerOne);
+            Controls.Add(scoreRightSide);
+            Controls.Add(scoreLeftSide);
+            ForeColor = Color.Transparent;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(2, 2, 2, 2);
+            MaximumSize = new Size(1540, 873);
+            MinimumSize = new Size(1538, 824);
+            Name = "Game";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "w";
+            KeyDown += Game_KeyDown;
+            KeyUp += Game_KeyUp;
+            ((System.ComponentModel.ISupportInitialize)playerOne).EndInit();
+            ((System.ComponentModel.ISupportInitialize)playerTwo).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pongBall).EndInit();
+            ((System.ComponentModel.ISupportInitialize)sep1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)sep2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)sep3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)sep4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)sep5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)sep6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)sep7).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
